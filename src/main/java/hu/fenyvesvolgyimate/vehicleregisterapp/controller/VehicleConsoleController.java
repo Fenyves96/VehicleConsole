d4a2a5a14d8ba008ec496acd88bff2592204a8c8
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2023-01-23 20:36:36 +0100

    separate VehicleWriter from VehicleReader

--- src/main/java/hu/fenyvesvolgyimate/vehicleregisterapp/controller/VehicleConsoleController.java
+++ src/main/java/hu/fenyvesvolgyimate/vehicleregisterapp/controller/VehicleConsoleController.java
@@ -1,25 +1,28 @@
 package hu.fenyvesvolgyimate.vehicleregisterapp.controller;
 
 import hu.fenyvesvolgyimate.vehicleregisterapp.dto.VehicleDTO;
-import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.VehicleRegister;
+import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.VehicleReader;
+import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.VehicleWriter;
 import hu.fenyvesvolgyimate.vehicleregisterapp.parser.VehicleDTOParser;
 
 public class VehicleConsoleController implements VehicleController {
-    VehicleRegister vehicleRegister;
+    VehicleWriter vehicleWriter;
+    VehicleReader vehicleReader;
     VehicleDTOParser parser = new VehicleDTOParser();
 
-    public VehicleConsoleController(VehicleRegister vehicleRegister){
-        this.vehicleRegister = vehicleRegister;
+    public VehicleConsoleController(VehicleWriter vehicleWriter, VehicleReader vehicleReader){
+        this.vehicleWriter = vehicleWriter;
+        this.vehicleReader = vehicleReader;
     }
 
     @Override
     public void getVehicleByRegistrationNumber(String registrationNumber) {
-        vehicleRegister.getVehicleByRegisterNumber("{\"registrationNumber\": \"%s\"}".formatted(registrationNumber));
+        vehicleReader.getVehicleByRegisterNumber("{\"registrationNumber\": \"%s\"}".formatted(registrationNumber));
     }
 
     @Override
     public void registerVehicle(VehicleDTO vehicle) {
         String requestJson = parser.parseVehicleDTOToJson(vehicle);
-        vehicleRegister.register(requestJson);
+        vehicleWriter.register(requestJson);
     }
 }
